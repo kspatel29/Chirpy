035059306aa37621e02bea0bb8b944ec66d9be1e
happyabuse47 <kushpatel2627@gmail.com>
2024-08-24 11:25:35 -0600

    created a server

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+        // Create a new http.ServeMux
+        mux := http.NewServeMux()
+
+        // Create a new http.Server struct
+        server := &http.Server{
+                Addr:    ":8080",
+                Handler: mux,
+        }
+
+        // Start the server
+        err := server.ListenAndServe()
+        if err != nil {
+                panic(err)
+        }
+}
